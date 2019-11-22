6dd475971ddb18e8eb67c4c2a769453193ee5072
Moritz Brandstaetter <arriggon@gmail.com>
2019-11-22 11:13:04 +0100

    add locales

--- src/main/java/com/moritzb/swcmspringdemo/config/WebConfig.java
+++ src/main/java/com/moritzb/swcmspringdemo/config/WebConfig.java
@@ -1,8 +1,30 @@
 package com.moritzb.swcmspringdemo.config;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public String projectName() {
+        return "java-curriculum";
+    }
+
+    @Bean
+    public String applicationName() {
+        return projectName() + "-" + "demoApplication";
+    }
+
+    @Bean("messageSource")
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:locale/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setUseCodeAsDefaultMessage(true);
+        return messageSource;
+    }
 }
