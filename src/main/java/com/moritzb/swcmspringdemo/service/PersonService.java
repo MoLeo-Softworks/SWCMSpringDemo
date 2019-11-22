6a1a38eeccfa8ddd1d6695367bdf96ed0f8dfee5
Moritz Brandstaetter <arriggon@gmail.com>
2019-11-22 11:16:17 +0100

    add service

--- src/main/java/com/moritzb/swcmspringdemo/service/PersonService.java
+++ src/main/java/com/moritzb/swcmspringdemo/service/PersonService.java
@@ -1,9 +1,18 @@
 package com.moritzb.swcmspringdemo.service;
 
+import com.moritzb.swcmspringdemo.domain.Person;
 import com.moritzb.swcmspringdemo.persistence.PersonRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
 
 @Service
+@Transactional(readOnly = true)
 public class PersonService {
 
     private final PersonRepository personRepository;
@@ -11,4 +20,14 @@ public class PersonService {
     public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
+
+    @Transactional(readOnly = false)
+    public Person createPerson(@NotNull @Valid Person p) {
+        return personRepository.save(p);
+    }
+
+    public List<Person> findPersonByLastNamePart(@NotEmpty String name) {
+        return personRepository.findByLastName(name);
+    }
+
 }
